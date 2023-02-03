cc19fcd444bf62c94522c14d17e7bd1f9fd49f47
Maciej Kuć <Maciej.Kuc@symfonia.pl>
2023-02-03 09:34:10 +0100

    Add User modules to GIT

---
+++ Modules/Modules.User/UserModule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Modules.User.Views;
+using PrismRoles.Core;
+
+namespace Modules.User
+{
+    [Roles("User")]
+    public class UserModule : IModule
+    {
+        IRegionManager _regionManager;
+        public UserModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(UserView));
+        }
+    }
+}
